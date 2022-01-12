ba477670e744be0efc8f0393d6775e53741bb7f2
Hannah <gkssk8827@gmail.com>
2022-01-12 09:19:16 +0900

    simple login - register

--- app.py
+++ app.py
@@ -6,16 +6,14 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
-
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('내AWS아이피', 27017, username="아이디", password="비밀번호")
-db = client.dbsparta_plus_week4
-
+client = MongoClient('3.35.0.78', 27017, username="test", password="test")
+db = client.sns_4w
 
 @app.route('/')
 def home():
@@ -53,24 +51,47 @@ def user(username):
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     # 로그인
-    return jsonify({'result': 'success'})
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
 
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
-    # 회원가입
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    # DB에 저장
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
     return jsonify({'result': 'success'})
-
 
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
-    # ID 중복확인
-    return jsonify({'result': 'success'})
-
+    username_receive = request.form['username_give']
+    print(db.users.find_one({"username": username_receive}))
+    exists = bool(db.users.find_one({"username": username_receive}))
+    print(exists)
+    return jsonify({'result': 'success', 'exists': exists})
 
 @app.route('/update_profile', methods=['POST'])
 def save_img():
@@ -114,7 +135,6 @@ def update_like():
         return jsonify({"result": "success", 'msg': 'updated'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
